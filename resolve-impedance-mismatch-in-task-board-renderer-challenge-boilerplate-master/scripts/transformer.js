56eddec64c24f68094a098f5065cc0b99437f9b2
jananikangesan <janakanges1996@gmail.com>
2023-03-24 16:03:29 +0530

    assignment2

--- resolve-impedance-mismatch-in-task-board-renderer-challenge-boilerplate-master/scripts/transformer.js
+++ resolve-impedance-mismatch-in-task-board-renderer-challenge-boilerplate-master/scripts/transformer.js
@@ -18,5 +18,62 @@ DO NOT MODIFY THE CODE IN OTHER FILES AS IT WILL IMPACT THE TEST OUTCOME AND BRO
 export const transform = (data) => {
 
     // Provide Solution Code Here
+   let output;
+//     if(typeof(data.boards)!='object' || typeof(data.lists) !='object' ||typeof(data.cards)!='object'|| typeof(data.comments)!='array'){
+
+//        return "Invalid Input Type, Input Type Must Be An Object with Array Type Boards, Lists, Cards and Comments Properties !!";
+//    }else{
+   
+         output=data.boards.filter(board => board.boardId )
+        .reduce((ExpectedOutput,CurrentOutput)=>{
+        let current=ExpectedOutput.find(board=>board.boardId == CurrentOutput.boardId)
+
+            if(!current){
+                ExpectedOutput.push({
+                    boardId:CurrentOutput.boardId,
+                    boardTitle:CurrentOutput.boardTitle,
+                    lists:[]
+                }) 
+                let index=0;
+                let list=data.lists.filter(list=>list.boardId == CurrentOutput.boardId)
+                .map(list=>{
+
+                    
+
+                    ExpectedOutput[0].lists.push({
+                        listId:list.listId,
+                        listTitle:list.listTitle,
+                        cards:[]
+                        
+                    });
+                    //console.log(index);
+
+                    let card =data.cards.filter(card => card.listId == list.listId).map(card=>{
+                        let count =0;
+
+                        let comment = data.comments.filter(comment => comment.cardId == card.cardId).map(comment=>{
+                            count =count +1;
+
+                        });
+
+                        ExpectedOutput[0].lists[index].cards.push({
+                            cardId:card.cardId,
+                            cardTitle:card.cardTitle,
+                            comments:count  
+                        });
+                    })
+                    index=index+1;
+                });
+    
+            }
+            return ExpectedOutput;
+        },[]);
+
+    //}
+       
+    return output;
+
+
+
     
 };
