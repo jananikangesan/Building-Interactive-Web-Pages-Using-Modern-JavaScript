18e1b9d271c37de0587cf9b107045744b845d0b4
jananikangesan <janakanges1996@gmail.com>
2023-03-31 12:13:28 +0530

    files

---
+++ develop-tic-tac-toe-game-master/js/script.js
@@ -0,0 +1,34 @@
+// button click handler
+const play = (event) => {
+
+//apply event to generate new game state
+
+//console.log(btn);
+
+
+// game state renderer renders the generated game state
+
+// renders text on button clicked with X or O
+
+// disable the button clicked
+
+// update panel values such as Turn Played By and Moves Left
+
+// reset panel values to default values
+
+// implement logic to get the winner
+
+// announce winner
+}
+
+// REPLAY-MODE :: replay-game-button-clicked->fetches events recorded->apply event->generates new game state->render game state
+
+
+// reset game to start a new
+
+
+
+// bind events to clickable buttons
+function enableButtons() {}
+
+module.exports = play;
